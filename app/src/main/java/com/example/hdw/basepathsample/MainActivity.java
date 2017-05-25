4aebe47bd436323529742e0ee9dc8d31e8e9de77
youlemei133 <914493278@qq.com>
2017-05-25 23:36:47 +0800

    使用PathMeasure实现搜索效果

--- app/src/main/java/com/example/hdw/basepathsample/MainActivity.java
+++ app/src/main/java/com/example/hdw/basepathsample/MainActivity.java
@@ -7,14 +7,18 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     private WaveView waveView;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        waveView =(WaveView)findViewById(R.id.waveView);
+
+        searchView =(SearchView)findViewById(R.id.searchView);
     }
     public void wave(View view){
-        waveView.doAnimation();
+//        waveView.doAnimation();
+        searchView.doAnimation();
     }
 }
--- app/src/main/java/com/example/hdw/basepathsample/SearchView.java
+++ app/src/main/java/com/example/hdw/basepathsample/SearchView.java
@@ -1,5 +1,9 @@
 package com.example.hdw.basepathsample;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -10,6 +14,7 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
 
 /**
  * Created by Administrator on 2017/05/24 0024.
@@ -21,6 +26,14 @@ public class SearchView extends View {
     private Paint mPaint;
     private Path mPath;
     private final int mRadius=50;
+    private float mCircleStartD;
+    private float mCircleStopD;
+    private int mCircleLen;
+    private float mLineStartD;
+    private float mLineStopD;
+    private int mLineLen;
+    private int mRotate;
+    private int mSingleCircleTime;
     public SearchView(Context context) {
         super(context);
     }
@@ -33,23 +46,105 @@ public class SearchView extends View {
         mPaint.setColor(Color.RED);
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeWidth(5);
-        mPathMeasure = new PathMeasure();
 
         mPath.addCircle(0,0,mRadius, Path.Direction.CW);
         mPath.moveTo(mRadius,0);
         mPath.rLineTo(mRadius,0);
+        mPathMeasure = new PathMeasure(mPath,true);
+        mCircleLen = (int) mPathMeasure.getLength();
+        mPathMeasure.nextContour();
+        mLineLen = (int) mPathMeasure.getLength();
+        mCircleStopD = mCircleLen;
+        mLineStopD = mLineLen;
+        mRotate = 45;
+        mSingleCircleTime = 1000;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.translate(canvas.getWidth()/2,canvas.getHeight()/2);
-        canvas.rotate(45);
+        canvas.rotate(mRotate);
 
         mPathMeasure.setPath(mPath,true);
-        mPathMeasure.getSegment(100,mPathMeasure.getLength(),mDstPath,true);
+
+
+        mPathMeasure.getSegment(mCircleStartD,mCircleStopD,mDstPath,true);
         mPathMeasure.nextContour();
-        mPathMeasure.getSegment(0,mPathMeasure.getLength(),mDstPath,true);
+
+
+        mPathMeasure.getSegment(mLineStartD,mLineStopD,mDstPath,true);
         canvas.drawPath(mDstPath,mPaint);
+        mDstPath.reset();
+    }
+
+    public void doAnimation(){
+        ValueAnimator oneAnimator =ValueAnimator.ofInt(0,360)
+                .setDuration(mSingleCircleTime);
+        oneAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int angle = (int) animation.getAnimatedValue();
+                mCircleStartD = mCircleLen* angle*1.0f/360;
+                mCircleStopD = mCircleLen;
+                mLineStartD = 0;
+                mLineStopD = mLineLen;
+                invalidate();
+            }
+        });
+
+        ValueAnimator twoAnimator =ValueAnimator.ofInt(0,mLineLen)
+                .setDuration((long) (mSingleCircleTime*(mLineLen*1.0f/mCircleLen)));
+        twoAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int len = (int) animation.getAnimatedValue();
+                mCircleStartD = 0;
+                mCircleStopD = len;
+                mLineStartD = 0;
+                mLineStopD = mLineLen - len;
+                invalidate();
+            }
+        });
+
+        ValueAnimator threeAnimator =ValueAnimator.ofInt(mRotate,360+mRotate)
+                .setDuration(mSingleCircleTime);
+        threeAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int rotate = (int) animation.getAnimatedValue();
+                mRotate=rotate;
+                invalidate();
+            }
+        });
+        threeAnimator.setRepeatCount(2);
+
+        ValueAnimator fourAnimator =ValueAnimator.ofInt(mLineLen,mCircleLen)
+                .setDuration(mSingleCircleTime);
+        fourAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int len = (int) animation.getAnimatedValue();
+                mCircleStartD = 0;
+                mCircleStopD = len;
+                invalidate();
+            }
+        });
+
+        ValueAnimator fiveAnimator =ValueAnimator.ofInt(0,mLineLen)
+                .setDuration(mSingleCircleTime);
+        fiveAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int len = (int) animation.getAnimatedValue();
+                mLineStartD = 0;
+                mLineStopD = len;
+                invalidate();
+            }
+        });
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playSequentially(oneAnimator,twoAnimator,threeAnimator,fourAnimator,fiveAnimator);
+        animatorSet.start();
     }
 }
